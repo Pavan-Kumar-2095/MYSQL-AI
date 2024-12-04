6c478e5b80a296b79200ac6cbc8e1b9f5d56408b
Pavan-Kumar-2095 <developer0exe@gmail.com>
2024-12-04 13:05:53 +0530

    update css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Input from "./Components/Input"
-import Update from "./Components/UnderDev"
+// import Update from "./Components/UnderDev"
 
 function App() {
 
 
   return (
   <div>
-     {/* <Input/> */}
-     <Update/>
+     <Input/>
+     {/* <Update/> */}
   </div>
            
   );
